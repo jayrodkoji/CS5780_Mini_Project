edd7de5cb5a5e0d7ed6b4e831d81114466fe44b7
jayrodkoji <jayrodkojI@gmail.com>
2020-04-11 10:42:14 -0600

    few changes

--- Src/main.c
+++ Src/main.c
@@ -106,7 +106,6 @@ int main(void)
   init_L3GD20();
 
   // Initialize LTDC
-  RCC->APB2ENR |= RCC_APB2ENR_LTDCEN;
   // Add alternate function GPIOs for LTDC
   // GPIOA 3(AF14) 4(AF14) 6(AF14) 11(AF14) 12(AF14)
   // GPIOB 0(AF9) 1(AF9) 8(AF14) 9(AF14) 10(AF14) 11(AF14)
@@ -148,6 +147,10 @@ int main(void)
   GPIOG->AFR[0] |= 0xEE000000;
   GPIOG->AFR[1] |= 0x0009E900;
 
+  // Enable the LTDC clock in the RCC register
+  RCC->APB2ENR |= RCC_APB2ENR_LTDCEN;
+  // Configure the required Pixel clock following the panel datasheet
+
 
   // LCD constants
   const uint16_t LCD_WIDTH = 240;
@@ -171,21 +174,6 @@ int main(void)
   LTDC->AWCR = (ACTIVE_W << 16) | ACTIVE_H;
   LTDC->TWCR = (TOTAL_W << 16) | TOTAL_H;
   LTDC->BCCR = 0xFF << LTDC_BCCR_BCRED_Pos; // RED
-  LTDC_Layer1->DCCR = 0xFF << LTDC_LxDCCR_DCALPHA_Pos | 0xFF << LTDC_LxDCCR_DCGREEN_Pos;
-    
-  /* configure layer 1  From http:\//www.lucadavidian.com/2017/10/02/stm32-using-the-ltdc-display-controller/*/
-  LTDC_Layer1->WHPCR = 269 << LTDC_LxWHPCR_WHSPPOS_Pos | 30 << LTDC_LxWHPCR_WHSTPOS_Pos;
-  LTDC_Layer1->WVPCR = 323 << LTDC_LxWVPCR_WVSPPOS_Pos | 4 << LTDC_LxWVPCR_WVSTPOS_Pos;
-  LTDC_Layer1->PFCR = 0x01;
-  //LTDC_Layer1->CFBAR = (uint32_t)framebuffer;
-    
-  LTDC_Layer1->CFBLR = 240 * 3 << LTDC_LxCFBLR_CFBP_Pos | 240 * 3 + 3 << LTDC_LxCFBLR_CFBLL_Pos;
-  LTDC_Layer1->CFBLNR = 320;
-    
-  LTDC_Layer1->DCCR = 0xFF << LTDC_LxDCCR_DCALPHA_Pos | 0xFF << LTDC_LxDCCR_DCGREEN_Pos;
-  LTDC_Layer1->CACR = 255;
-  LTDC_Layer1->CR |= LTDC_LxCR_LEN;
-  // end layer1 config
 
   LTDC->SRCR = LTDC_SRCR_IMR;
   /* USER CODE END 2 */
@@ -232,7 +220,6 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct;
 
   /** Configure the main internal regulator output voltage 
   */
@@ -262,12 +249,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /* initialize LTDC LCD clock */
-  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
-  PeriphClkInitStruct.PLLSAI.PLLSAIN = 60;
-  PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
-  PeriphClkInitStruct.PLLSAIDivR = RCC_PLLSAIDIVR_4;
-  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 }
 
 /* USER CODE BEGIN 4 */
